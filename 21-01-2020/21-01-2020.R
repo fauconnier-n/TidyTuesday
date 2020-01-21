1ff5dd9ba9009cf33ac8e2dbd518744a95bcf745
fauconnier-n <36164501+fauconnier-n@users.noreply.github.com>
2020-01-21 18:09:39 +0100

    Update 21-01-2020.R

--- 21-01-2020/21-01-2020.R
+++ 21-01-2020/21-01-2020.R
@@ -3,34 +3,34 @@ library(tidyverse)
 #Load the data
 spotify_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv')
 
-#rapid look at the data and the variables used here
+#A rapid look at the data and the variables used here
 summary(spotify_songs)
 table(spotify_songs$playlist_genre)
 table(spotify_songs$playlist_subgenre)
 table(spotify_songs$mode) # 1=major, 0=minor
 table(spotify_songs$key) # 0=C, 1=C#, etc
 
-#change mode and key as factors
+#mode and key as categorical variables
 spotify_songs$mode <- factor(spotify_songs$mode)
 spotify_songs$key <- factor(spotify_songs$key)
 
-#plot 1: songs and mode count per genre
+#plot 1: song and mode count per genre
 mode_genre <- ggplot(spotify_songs, aes(fill=mode, x=playlist_genre)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   geom_bar(position='stack')
 
-#plot 2: songs and mode count per subgenre
+#plot 2: song and mode count per subgenre
 mode_subgenre <- ggplot(spotify_songs, aes(fill=mode, x=playlist_subgenre)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   geom_bar(position='stack')
 
-#plot 3: songs and key count per genre
+#plot 3: song and key count per genre
 key_genre <- ggplot(spotify_songs, aes(fill=key, x=playlist_genre)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   geom_bar(position='stack')
 
 
-#plot 4: songs and key count per subgenre
+#plot 4: song and key count per subgenre
 key_subgenre <- ggplot(spotify_songs, aes(fill=key, x=playlist_subgenre)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   geom_bar(position='stack')
